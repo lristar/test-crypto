be6317e663ae2995872ea76bc6756d5210029c37
LiZhuYou <84826607@11.com>
2022-01-13 21:53:50 +0800

    添加测试

--- ellipticCurve/ellipticCureve_test.py
+++ ellipticCurve/ellipticCureve_test.py
@@ -2,18 +2,20 @@ import time
 
 
 class EllipticCurve:
-    def __init__(self, a, b,p):
+    def __init__(self, a, b, p):
         self.a = a
         self.b = b
         self.p = p
 
     def point(self, x, y):
         return Point(x, y, self.a, self.b, self.p)
+
     @staticmethod
     def include(point):
-        if (point.y**2) % point.p == ((point.x**3)+(point.a*point.x)+point.b)%point.p:
+        if (point.y ** 2) % point.p == ((point.x ** 3) + (point.a * point.x) + point.b) % point.p:
             return True
         return False
+
 
 class Point:
     def __init__(self, x, y, a, b, p):
@@ -24,27 +26,26 @@ class Point:
         self.p = p
 
     def __add__(self, other):
-        if self.x == other.x and (self.y+other.y) % self.p==0:
+        if self.x == other.x and (self.y + other.y) % self.p == 0:
             return self.__class__(0, 0, self.a, self.b, self.p)
         if self.x == 0 and self.y == 0:
             return other
         if self.x != other.x or self.y != other.y:
-            k = self.modAll((self.y - other.y),(self.x - other.x),self.p)
+            k = self.modAll((self.y - other.y), (self.x - other.x), self.p)
         else:
-            k = self.modAll((3*(self.x**2)+self.a), 2*self.y, self.p)
+            k = self.modAll((3 * (self.x ** 2) + self.a), 2 * self.y, self.p)
         x3 = (k ** 2 - (self.x + other.x)) % self.p
         y3 = ((k * (self.x - x3)) - self.y) % self.p
         return self.__class__(x3, y3, self.a, self.b, self.p)
 
-
     def modAll(self, x, y, m):
         if x % y != 0:
-            return x % m * self.quickM(y,m-2,m)
+            return x % m * self.quickM(y, m - 2, m)
         else:
-            return (x/y) % m
+            return (x / y) % m
 
     @staticmethod
-    def quickM(a,b,m):
+    def quickM(a, b, m):
         a = a % m
         ans = 1
         while b != 0:
@@ -63,17 +64,14 @@ class Point:
         # }
 
 
-
-
 def main():
-     x = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
-     y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
-     p = 2 ** 256 - 2 ** 32 - 977
-     a = EllipticCurve(0, 7, p)
-     p1 = a.point(x, y)
-     p1 = p1 +p1
-     print(a.include(p1))
-
+    x = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+    y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+    p = 2 ** 256 - 2 ** 32 - 977
+    a = EllipticCurve(0, 7, p)
+    p1 = a.point(x, y)
+    p1 = p1 + p1
+    print(a.include(p1))
 
 
 if __name__ == '__main__':
