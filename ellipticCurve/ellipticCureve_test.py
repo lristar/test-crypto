99761210c013adeaa7705431157fbe58fc1b9d57
LiZhuYou <84826607@11.com>
2022-01-13 21:43:45 +0800

    优化curve

--- ellipticCurve/ellipticCureve_test.py
+++ ellipticCurve/ellipticCureve_test.py
@@ -9,7 +9,11 @@ class EllipticCurve:
 
     def point(self, x, y):
         return Point(x, y, self.a, self.b, self.p)
-
+    @staticmethod
+    def include(point):
+        if (point.y**2) % point.p == ((point.x**3)+(point.a*point.x)+point.b)%point.p:
+            return True
+        return False
 
 class Point:
     def __init__(self, x, y, a, b, p):
@@ -32,21 +36,43 @@ class Point:
         y3 = ((k * (self.x - x3)) - self.y) % self.p
         return self.__class__(x3, y3, self.a, self.b, self.p)
 
+
     def modAll(self, x, y, m):
-        if y == 0:
-            return 0
         if x % y != 0:
-            return (x*(y**(m-2))) % m
+            return x % m * self.quickM(y,m-2,m)
         else:
             return (x/y) % m
 
+    @staticmethod
+    def quickM(a,b,m):
+        a = a % m
+        ans = 1
+        while b != 0:
+            if b & 1:
+                ans = (ans * a) % m
+            b >>= 1
+            a = (a * a) % m
+        return ans
+
+        # for b.Int64() != 0 {
+        # if b.Int64() & 1 == 1 {
+        # res.Mul(res, a).Mod(res, p)
+        # }
+        # b = b.Rsh(b, 1)
+        # a.Mul(a, a).Mod(a, p)
+        # }
+
+
+
+
 def main():
-     a = EllipticCurve(8, 7, 73)
-     p1 = a.point(32, 53)
-     p2 = a.point(32, 53)
-     for i in range(1, 15):
-         p1 = p1 + p2
-         print("x=", p1.x, "y=", p1.y, "a=", p1.a, "b=", p1.b, "p", p1.p)
+     x = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+     y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+     p = 2 ** 256 - 2 ** 32 - 977
+     a = EllipticCurve(0, 7, p)
+     p1 = a.point(x, y)
+     p1 = p1 +p1
+     print(a.include(p1))
 
 
 
